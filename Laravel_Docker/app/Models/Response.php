ff11a29ea9bb591d6bf6fa499bdd90837f9dadfe
Kabos0809 <hiyamazu70@gmail.com>
2023-03-05 03:06:41 +0900

    #8 Responseモデル作成

--- Laravel_Docker/app/Models/Response.php
+++ Laravel_Docker/app/Models/Response.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Response extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'text'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class)
+    }
 }
