7eba69a01c029f9e908d78af5c33b43e0c335adc
Kabos0809 <hiyamazu70@gmail.com>
2023-03-06 13:51:49 +0900

    #6 ページネーション追加

--- Laravel_Docker/resources/views/components/thread-item.blade.php
+++ Laravel_Docker/resources/views/components/thread-item.blade.php
@@ -1,10 +1,14 @@
 <div>
     @props(['thread' => $thread])
-    <a href="{{ route('threads.detail', $thread) }}" class="p-4 block grid bg-white sm:rounded-lg border-1 shadow-sm">
-        <span>
+    <a href="{{ route('threads.detail', $thread) }}" class="p-4 block grid bg-white sm:rounded-lg border-1 shadow hover:bg-gray-100 duration-100">
+        <span class="text-xl mb-1 ml-1">
             {{ $thread->title }}
         </span>
-        <span class="text-gray-600 text-sm">
+        <hr/>
+        <span class="text-gray-800 ml-3 mt-1 mb-1 text-sm">
+            {{ $thread->text }}
+        </span>
+        <span class="text-gray-600 text-sm ml-1">
             {{ $thread->created_at->diffForHumans() }}
         </span>
     </a>
